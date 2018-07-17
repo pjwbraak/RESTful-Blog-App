cc0f3160710092832e3984f287da9d6352533870
Pieter <censornv@gmail.com>
2018-07-17 11:59:34 +0000

    show blogs of user

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express     = require("express"),
     router      = express.Router(),
     User        = require("../models/user"),
+    Blog        = require("../models/blog"),
     middleware  = require("../middleware");
 
 //SHOW - show profile for logged in user (private)
@@ -19,5 +20,45 @@ router.get("/:id/private", middleware.isLoggedIn, function(req, res){
             res.redirect("/blogs");
         }
 });
+
+//show one user's posts
+router.get("/:id/blogs", function(req, res){
+       User.findById(req.params.id, function(err, foundUser){
+            if(err){
+               console.log(err); //user not found
+               res.redirect("/");
+            } else {
+                Blog.find({ "author.id": req.params.id }, function(err, blogs){
+                if(err){
+                    console.log(err);
+                } else {
+                    res.render("./users/blogs", {user:foundUser, blogs: blogs});
+                }
+            });
+        }
+    });
+});
+
+// router.get("/:id/blogs", middleware.isLoggedIn, function(req, res){
+//     if(req.user._id.equals(req.params.id)){
+//       User.findById(req.params.id, function(err, foundUser){
+//             if(err){
+//               console.log(err);
+//               res.redirect("/");
+//             } else {
+//                 Blog.find({ "author.id": foundUser._id }, function(err, blogs){
+//                 if(err){
+//                     console.log(err);
+//                 } else {
+//                     res.render("./users/blogs", {user:foundUser, blogs: blogs});
+//                 }
+//             });
+//         }
+//     });
+//     } else { 
+//             req.flash("error", "No permission to access page");
+//             res.redirect("/blogs");
+//         }
+// });
 
 module.exports = router;
